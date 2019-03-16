725ea511b7de0f0015e2682d9a0acd31932427e4
PENA5 <44989115+PENA5@users.noreply.github.com>
2019-03-16 05:18:20 -0500

    README.md

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,32 +2,32 @@ var friends = [
 
 {
     "name":"Frank 'The Tank' Ricard",
-    "photo":"",
+    "photo":"app/data/images/frank-the-tank.jpg",
     "scores":[5,4,3,4,4,5,4,3,3,4]
   },
   {
     "name":"Mitch 'The Godfather' Martin",
-    "photo":"",
+    "photo":"app/data/images/the-godfather.jpg",
     "scores":[5,4,3,3,4,3,3,4,5,4]
   },
   {
     "name":"Benard 'Beanie'",
-    "photo":"",
+    "photo":"app/data/images/beanie.jpg",
     "scores":[4,3,3,2,4,3,4,3,4,5]
   },
   {
     "name":"Weensie",
-    "photo":"",
+    "photo":"app/data/images/weensie.jpg",
     "scores":[3,3,2,3,1,3,2,3,2,2]
   },
   {
     "name":"Dean 'Cheese' Gordon",
-    "photo":"",
+    "photo":"app/data/images/cheese.jpg",
     "scores":[1,2,3,1,1,3,2,1,1,1]
   },
   {
     "name":"Joseph 'Your Boy Blue' Palasky",
-    "photo":"",
+    "photo":"app/data/images/blue.jpg",
     "scores":[5,5,3,4,5,4,3,4,3,5]
   },
 
